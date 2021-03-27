3ec51b9ac768a20604f6eadbda1559552e43758e
jpcui <624498030@qq.com>
2021-03-27 22:58:58 +0800

    Initial commit

---
+++ src/pages/index/index.tsx
@@ -0,0 +1,36 @@
+import {Component} from 'react'
+import {View} from '@tarojs/components'
+
+import "taro-ui/dist/style/components/button.scss" // 按需引入
+import './index.scss'
+import JpCalendar from "../components/calendar/calendar";
+import * as React from "react";
+import Calendar from "taro-ui/types/calendar";
+import * as dayjs from "dayjs";
+import {AtCalendar} from "taro-ui";
+
+export default class Index extends Component {
+
+  componentWillMount () { }
+
+  componentDidMount () { }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  render () {
+    const selectedDate: Calendar.SelectedDate = {
+      start: dayjs().toDate(),
+      end: dayjs().add(10, "day").toDate()
+    };
+    return (
+      <View className='index'>
+        <AtCalendar selectedDate={selectedDate} />
+        <JpCalendar />
+      </View>
+    )
+  }
+}
