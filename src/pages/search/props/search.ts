7fbaa071c72960e4ffd9a42de728adf0be9e3efa
jpcui <624498030@qq.com>
2021-04-02 19:11:43 +0800

    finish header

---
+++ src/pages/search/props/search.ts
@@ -0,0 +1,15 @@
+import Calendar from "../../components/calendar/props/calendar";
+
+export interface CityInfo {
+  province: string;
+  city: string;
+  district: string;
+}
+
+export interface SearchProps {
+  onSelectCity: (cityInfo: CityInfo) => void;
+}
+
+export interface SearchState {
+  keyword: string;
+}
